5735d3d4a9b7338d781e14fc352952c3b781991d
Chu Tuan Phuong <phuong.ctp15@gmail.com>
2025-01-19 15:28:09 +0700

    darkmode

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Button from '@mui/material/Button'
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
+import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
+import { Home } from '@mui/icons-material'
+import { pink } from '@mui/material/colors'
+import { useColorScheme } from '@mui/material/styles'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import LightModeIcon from '@mui/icons-material/LightMode'
+import DarkModeIcon from '@mui/icons-material/DarkMode'
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
 
-function App() {
-  const [count, setCount] = useState(0)
+function SelectSmall() {
+  const { mode, setMode } = useColorScheme('light')
+  const handleChange = (e) => {
+    setMode(e.target.value)
+  }
 
   return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="label-select-color-mode">Mode</InputLabel>
+      <Select
+        labelId="label-select-color-mode"
+        id="select-color-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value="light">
+          <div style={{ display: 'flex', alignItems: 'center', gap: '4px' }}>
+            <LightModeIcon fontSize="small" /> Light
+          </div>
+        </MenuItem>
+        <MenuItem value="dark">
+          <div style={{ display: 'flex', alignItems: 'center', gap: '4px' }}>
+            <DarkModeIcon fontSize="small" /> Dark
+          </div>
+        </MenuItem>
+
+        <MenuItem value="system">
+          <div style={{ display: 'flex', alignItems: 'center', gap: '4px' }}>
+            <SettingsBrightnessIcon fontSize="small" /> System
+          </div>
+        </MenuItem>
+      </Select>
+    </FormControl>
+  )
+}
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
+
+function App() {
+  return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <SelectSmall />
+      <hr />
+      <ModeToggle />
+      <hr />
+      <Button variant="contained">Hello world</Button>
+      <AccessAlarmIcon />
+      <ThreeDRotation />
+      <Home sx={{ color: pink[500] }} />
     </>
   )
 }
